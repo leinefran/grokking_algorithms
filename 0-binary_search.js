bc9fbd3618748fadfa230c2bcdb0344af1ba04ff
leinefran <penny.leine@gmail.com>
2019-04-23 23:49:44 +0000

    Binary Search: first commit

---
+++ 0-binary_search.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/* implementation of Binary Search in JavaScript */
+
+function binary_search(list, item){
+
+    let low = 0;
+    let length = list.length;
+    let high = length - 1;
+
+    while (low <= high){
+	let mid = (low + high);
+	guess = list[mid]
+	if (guess == item){
+	    return mid;
+	} else if (guess > item){
+	    high = mid - 1;
+	} else {
+	    low = mid + 1;
+	}
+    }
+    return None;
+}
