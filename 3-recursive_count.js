94a833634b6d54b343ed78dcd754a0e60962b769
leinefran <penny.leine@gmail.com>
2019-05-02 02:02:43 +0000

    First draft: a recursive function to count the number of items in an array

---
+++ 3-recursive_count.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* A recursive function to count the number of items in an array */
+
+let count = function count(array){
+
+    if (array.length === 0)
+	return 0;
+
+    return 1 + count(array.slice(1));
+}
