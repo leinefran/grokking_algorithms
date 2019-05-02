3a1cf1b920071d2cb058572f1f2ef7189f180805
leinefran <penny.leine@gmail.com>
2019-05-02 02:25:57 +0000

    Added: export to make the function visible from the outside. Changed: return statement. Fixed: style

--- 2-recursive_sum.js
+++ 2-recursive_sum.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 /* implementation of Array Sum using recursion */
 
-let sum = function sum(array){
+exports.sum = function (array) {
+  let sum = 0;
+  if (array.length === 0) {
+    return 0;
+  }
 
-    if (array.length === 0)
-	return 0;
-
-    /* array.slice(1) creates a shallow copy of the array starting from the first element onwards, and no mutation ever occurs on the original array. */
-    return array[0] + sum(array.slice(1));
-
-}
+  /* array.slice(1) creates a shallow copy of the array starting from the first element onwards, and no mutation ever occurs on the original array. */
+  return array[0] + sum(array.slice(1));
+};
