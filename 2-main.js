3f68204efb0951030f16b5d3c2579d7f187c9327
leinefran <penny.leine@gmail.com>
2019-05-02 02:41:49 +0000

    Changed: body of function

---
+++ 2-main.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+const sum = require('./2-recursive_sum').sum;
+console.log(sum(3, 5));
--- 2-recursive_sum.js
+++ 2-recursive_sum.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 /* implementation of Array Sum using recursion */
 
-exports.sum = function (array) {
-  let sum = 0;
-  if (array.length === 0) {
+exports.sum = function (array, n) {
+  if (array.length === n) {
     return 0;
   }
-
-  /* array.slice(1) creates a shallow copy of the array starting from the first element onwards, and no mutation ever occurs on the original array. */
-  return array[0] + sum(array.slice(1));
-};
+  return array[n] + sum(array, n + 1);
+}
