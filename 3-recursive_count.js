ec6b0213288bd39be13a9fc0a0926ba680a9bf94
leinefran <penny.leine@gmail.com>
2019-05-21 03:06:11 +0000

    Changed: if condition and return value

--- 3-recursive_count.js
+++ 3-recursive_count.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 /* A recursive function to count the number of items in an array */
 
-let count = function count(array){
+let count = function count (array) {
+  if (array.length === 0) { return 0; }
 
-    if (array.length === 0)
-	return 0;
-
-    return 1 + count(array.slice(1));
-}
+  return 1 + count(array.slice(1));
+};
